2d2f88aa5a6adf432475e3048772c0a3d3b31d7b
Denis Leal <denisleal@msn.com>
2020-02-11 15:17:04 -0300

    User insert

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.example.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.course.entities.User;
 import com.example.course.services.UserService;
@@ -25,10 +29,21 @@ public class UserResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{id}") 
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	// usa-se o método HTTP GET para recuperar informações do BD, para os outros tipos do CRUD é preciso usar o POST
+	
+	@PostMapping
+	public ResponseEntity<User> insert (@RequestBody User obj){
+		obj = service.insert(obj);
+		//return ResponseEntity.ok().body(obj); // retorna a resposta 200 (OK) porém por se tratar de inserção, o mais correto é retornar o status 201 (inserted ok)
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+		
 	}
 
 }
